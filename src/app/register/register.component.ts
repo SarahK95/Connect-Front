a4743fcfdd9e361e3303e3b0577c67f822810a81
aust1inn <austinotieno364@gmail.com>
2022-07-05 11:01:58 +0300

    integrating frontend and backend

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from './../services/user.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
+  providers: [UserService]
 })
-export class RegisterComponent implements OnInit {
+export class  RegisterComponent implements OnInit {
+  register:any;
 
-  constructor() { }
+  constructor(private userService: UserService, private router: Router) {}
+  ngOnInit() {
+    this.register = {
+      username:'',
+      email:'',
+      password:'',
+      password2:'',
+    };
+ 
+}
 
-  ngOnInit(): void {
-  }
+registerUser(){
+
+  this.userService.newUserRegister(this.register).subscribe(
+    response => {
+      alert('User  ' + this.register.username + ' has been registered successfully!')
+      this.router.navigate(["login"])
+    },
+    error => console.log('error', error)
+  );
+
+}
+
+
 
 }
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+
+  newUserRegister(userData: any): Observable<any> {
+    return this.http.post('http://127.0.0.1:8000/signup/customer/', userData);
+}
+
+loginUser(userData: any): Observable<any> {
+  return this.http.post('http://127.0.0.1:8000/login/', userData);
+}
+
+}
